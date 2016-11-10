83a7195dcede3fcf5cffdd1d869c374f8d82c7ce
Jelle De Bock <debockjelle@student.hogent.be>
2016-11-10 11:13:48 +0100

    Delete 20.pl

--- Powershell/20.pl
+++
@@ -1,6 +0,0 @@
-﻿<#-----------------------------------------------------------------------------------------------
-    Toon alle methodes + methodequalifiers voor Win32_Process. De uitvoer afwerken zodat je alle 
-    qualifiers ook ziet is niet zo evident. Gebruik daarvoor foreach + Write-Host.
------------------------------------------------------------------------------------------------#>
-
-$server = New-Object
