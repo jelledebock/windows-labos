bdc54530e8e97443be2616d221ba3314f47a60a2
Jelle De Bock <debockjelle@student.hogent.be>
2016-11-10 11:13:58 +0100

    Rename 5.ps1 to 05.ps1

--- Powershell/5.ps1
+++
@@ -1,5 +0,0 @@
-﻿Get-Service | where {$_.Status -eq "Running"} | select Name;
-
-Get-Service | Group-Object -Property status;
-
-Get-Service | where {$_.DependentServices.Count -gt 1 } | select Name, @{Label="Aantal Afhankelijke Services";Expression={$_.dependentservices.count}};
