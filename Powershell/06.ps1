5134b2f22df57d415a690283712dbc8872ba2024
Jelle De Bock <debockjelle@student.hogent.be>
2016-11-10 11:14:08 +0100

    Rename 6.ps1 to 06.ps1

---
+++ Powershell/06.ps1
@@ -0,0 +1,1 @@
+﻿dir -File | where{$_.LastWriteTime -gt (Get-Date -Year 2016 -Month 10 -Day 1)} | Sort-Object -Property LastWriteTime -Descending
--- Powershell/6.ps1
+++
@@ -1,1 +0,0 @@
-﻿dir -File | where{$_.LastWriteTime -gt (Get-Date -Year 2016 -Month 10 -Day 1)} | Sort-Object -Property LastWriteTime -Descending
